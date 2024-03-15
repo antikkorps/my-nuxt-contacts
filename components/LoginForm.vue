ba8861f8ca25182abfb5b1c9a802d6b4e68176d1
Franck <franckvienot7@gmail.com>
2024-03-15 11:59:12 +0100

    add nextAuth basic configuration in the project

---
+++ components/LoginForm.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hero min-h-screen bg-base-200">
+    <div class="hero-content flex-col lg:flex-row-reverse">
+      <div class="text-center lg:text-left">
+        <h1 class="text-5xl font-bold">Connectez-vous maintenant!</h1>
+        <p class="py-6">
+          Pour accéder à l'ensemble de vos contacts, connectez-vous maintenant et profitez
+          de les avoir tous au même endroit.
+        </p>
+      </div>
+      <div class="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+        <form class="card-body">
+          <div class="form-control">
+            <label class="label">
+              <span class="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              placeholder="email"
+              class="input input-bordered"
+              required
+            />
+          </div>
+          <div class="form-control">
+            <label class="label">
+              <span class="label-text">Mot de passe</span>
+            </label>
+            <input
+              type="password"
+              placeholder="Mot de passe"
+              class="input input-bordered"
+              required
+            />
+            <label class="label">
+              <a href="#" class="label-text-alt link link-hover">Mot de passe oublié ?</a>
+            </label>
+          </div>
+          <div class="form-control mt-6">
+            <button class="btn btn-primary">Se connecter</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { resolve } from "node:path"
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode"]
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode", "@hebilicious/authjs-nuxt"],
+  alias: {
+    cookie: resolve(__dirname, "node_modules/cookie"),
+  },
+  runtimeConfig: {
+    authJs: {
+      secret: process.env.NUXT_NEXTAUTH_SECRET,
+    },
+    public: {
+      authJs: {
+        baseUrl: process.env.NUXT_NEXTAUTH_URL, // The URL of your deployed app (used for origin Check in production)
+        verifyClientOnEveryRequest: true, // whether to hit the /auth/session endpoint on every client request
+      },
+    },
+  },
 })

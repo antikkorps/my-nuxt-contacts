36b70f16f3c81353533c90994341d80dbddf2467
Franck <franckvienot7@gmail.com>
2024-03-15 10:32:57 +0100

    add prisma to the project

--- pages/mycontact.vue
+++ pages/mycontact.vue
@@ -1,6 +1,18 @@
+<script setup lang="ts">
+const { data } = await useFetch("/api/v1/hello")
+</script>
 <template>
   <div>
     <h1>My Contacts</h1>
     <p>Here are your contacts</p>
+
+    <pre>{{ data }}</pre>
+
+    <!-- <div v-if="data">
+      <ul>
+        <li v-for="contact in data.contacts" :key="contact.id">
+          {{ contact.name }}
+        </li>
+      </ul> -->
   </div>
 </template>

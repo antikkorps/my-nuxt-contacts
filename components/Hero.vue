81f49b69ae856877be227fdd6ed9d4ad249f14bb
Franck <franckvienot7@gmail.com>
2024-03-15 22:58:07 +0100

    made a change on Hero background

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="hero min-h-screen bg-base-200">
+    <div class="hero min-h-screen">
       <div class="hero-content text-center">
         <div class="max-w-md">
           <h1 class="text-5xl font-bold">Hello there</h1>

645ab2016a25e350d16aab6dfc090eca80e056bd
Franck <franckvienot7@gmail.com>
2024-03-23 22:44:55 +0100

    add logic to the api route [id]

--- server/api/v1/contacts/[id].ts
+++ server/api/v1/contacts/[id].ts
@@ -9,8 +9,6 @@ export default defineEventHandler(async (event) => {
   const session = await getServerSession(event, authOptions);
   const contactId = getRouterParam(event, "id");
 
-  return `hello ${contactId} !`;
-
   if (!session) {
     return {
       status: 401,
@@ -18,48 +16,55 @@ export default defineEventHandler(async (event) => {
     };
   }
 
-  // const userId = session.user?.id;
+  const userId = session?.user?.id;
 
-  // const { id } = event.query;
+  try {
+    const contact = await prisma.contact.findUnique({
+      where: {
+        id: contactId,
+        user: {
+          id: userId,
+        },
+      },
+    });
 
-  // try {
-  //   const contact = await prisma.contact.findUnique({
-  //     where: {
-  //       id,
-  //     },
-  //   });
+    if (!contact || contact.userId !== userId) {
+      return {
+        status: 404,
+        body: { error: "Contact not found" },
+      };
+    }
+    if (contact.userId !== userId) {
+      return {
+        status: 403,
+        body: { error: "Forbidden" },
+      };
+    }
 
-  //   if (!contact || contact.userId !== userId) {
-  //     return {
-  //       status: 404,
-  //       body: { error: "Contact not found" },
-  //     };
-  //   }
-
-  //   return {
-  //     status: 200,
-  //     body: {
-  //       contact: {
-  //         id: contact.id,
-  //         image: contact.image,
-  //         firstName: contact.firstName,
-  //         lastName: contact.lastName,
-  //         email: contact.email,
-  //         phone: contact.phone,
-  //         linkedin: contact.linkedin,
-  //         facebook: contact.facebook,
-  //         instagram: contact.instagram,
-  //         twitter: contact.twitter,
-  //         jobTitle: contact.jobTitle,
-  //         company: contact.company,
-  //         notes: contact.notes,
-  //       },
-  //     },
-  //   };
-  // } catch (error) {
-  //   return {
-  //     status: 500,
-  //     body: { error: "Internal server error" },
-  //   };
-  // }
+    return {
+      status: 200,
+      body: {
+        contact: {
+          id: contact.id,
+          image: contact.image,
+          firstName: contact.firstName,
+          lastName: contact.lastName,
+          email: contact.email,
+          phone: contact.phone,
+          linkedin: contact.linkedin,
+          facebook: contact.facebook,
+          instagram: contact.instagram,
+          twitter: contact.twitter,
+          jobTitle: contact.jobTitle,
+          company: contact.company,
+          notes: contact.notes,
+        },
+      },
+    };
+  } catch (error) {
+    return {
+      status: 500,
+      body: { error: "Internal server error" },
+    };
+  }
 });

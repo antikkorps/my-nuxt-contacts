291a5b13029737c4f28c7e0f5977475db2e5c4f2
Franck <franckvienot7@gmail.com>
2024-04-11 17:09:01 +0200

    add some tests

--- tests/my-test.nuxt.spec.ts
+++ tests/my-test.nuxt.spec.ts
@@ -1,5 +1,5 @@
 // @vitest-environment node
-import { test, expect } from "vitest";
+import { test, expect, vi } from "vitest";
 import { chromium } from "playwright-core";
 
 test("should load the main page", async () => {
@@ -29,6 +29,38 @@ test("should load the login page", async () => {
   expect(emailInput).toBeTruthy();
   expect(passwordInput).toBeTruthy();
   expect(loginButton).toBeTruthy();
+
+  await browser.close();
+});
+
+test("should login", async () => {
+  const browser = await chromium.launch();
+
+  const page = await browser.newPage();
+
+  await page.goto("http://localhost:3000/login");
+
+  await page.fill("input[type=email]", "testuser@example.com");
+  await page.fill("input[type=password]", "password");
+  await page.click("button[type=submit]");
+  // expect redirect to /mycontacts
+  await page.goto("http://localhost:3000/mycontacts");
+
+  await browser.close();
+});
+
+test("should go to profile page", async () => {
+  const browser = await chromium.launch();
+
+  const page = await browser.newPage();
+
+  await page.goto("http://localhost:3000/login");
+
+  await page.fill("input[type=email]", "testuser@example.com");
+  await page.fill("input[type=password]", "password");
+  await page.click("button[type=submit]");
+  // expect redirect to /profile
+  await page.goto("http://localhost:3000/profile");
 
   await browser.close();
 });
